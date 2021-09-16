8ee7e19a21ac6e4addd9550a9f432e77534af84e
Espen Waaga <espen.waaga@nav.no>
2021-09-16 09:20:52 +0200

    Oppdatere Constants.java

--- src/main/java/no/nav/foreldrepenger/common/util/Constants.java
+++ src/main/java/no/nav/foreldrepenger/common/util/Constants.java
@@ -9,10 +9,12 @@ public final class Constants {
     public static final String NAV_CONSUMER_TOKEN = "Nav-Consumer-Token";
     public static final String NAV_CALL_ID = "Nav-CallId";
     public static final String NAV_CALL_ID1 = "Nav-Call-Id";
+    public static final String NAV_CALL_ID2 = "callId";
     public static final String NAV_USER_ID = "Nav-User-Id";
     public static final String NAV_AKTØR_ID = "Nav-Aktør-Id";
     public static final String CALL_ID = "callId";
     public static final String ISSUER = "selvbetjening";
+    public static final String TOKENX = "tokenx";
     public static final String NAV_TOKEN_EXPIRY_ID = "Nav-Token-Expiry";
     public static final String NAV_PERSON_IDENT = "Nav-Personident";
     public static final String FNR = "fnr";
