f4d7ce78619265fa6f08524da8359594ef282775
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2022-03-03 10:10:39 +0100

    Utvider regex for fritekstfelt

--- src/main/java/no/nav/foreldrepenger/common/domain/validation/InputValideringRegex.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/validation/InputValideringRegex.java
@@ -2,7 +2,7 @@ package no.nav.foreldrepenger.common.domain.validation;
 
 public class InputValideringRegex {
 
-    public static final String FRITEKST = "^[0-9a-zA-ZæøåÆØÅAaÁáBbCcČčDdĐđEeFfGgHhIiJjKkLlMmNnŊŋOoPpRrSsŠšTtŦŧUuVvZzŽžéôèÉöüäÖÜÄ .'\\-/\n%§\\!?@_()+:;,=\"&]*$";
+    public static final String FRITEKST = "^[0-9a-zA-ZæøåÆØÅAaÁáBbCcČčDdĐđEeFfGgHhIiJjKkLlMmNnŊŋOoPpRrSsŠšTtŦŧUuVvZzŽžéôóèÉöüäÖÜÄ .'\\-/\n%§\\!?@_()+:;,=\"&]*$";
 
     private InputValideringRegex() {
         throw new IllegalAccessError("Skal ikke instansieres");
