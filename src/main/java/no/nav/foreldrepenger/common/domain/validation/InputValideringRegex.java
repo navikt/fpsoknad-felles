c18818f6e682197a991fd9367050ddaf8bba6caf
Espen Waaga <espen.waaga@nav.no>
2022-03-03 12:03:07 +0100

    Fransk Œ legges også til

--- src/main/java/no/nav/foreldrepenger/common/domain/validation/InputValideringRegex.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/validation/InputValideringRegex.java
@@ -2,7 +2,7 @@ package no.nav.foreldrepenger.common.domain.validation;
 
 public class InputValideringRegex {
 
-    public static final String FRITEKST = "^[0-9a-zA-ZÁáĄąÂâĀāĂăßČčĆćÇçĎďĐđÐðĔĕÉéĘęĖėÈèËëÊêĒēĢģİiĮįÍíÎîÏïĪīĶķŁłŊŋŇňŃńŅņŠšŚśŞşŤťŦŧŢţŲųŪūÚúŮůÝýŽžŹźŻżÕõÔôÓóÖöÜüÄäÆæØøÅå .'\\-/\n%§\\!?@_()+:;,=\"&]*$";
+    public static final String FRITEKST = "^[0-9a-zA-ZÁáĄąÂâĀāĂăßČčĆćÇçĎďĐđÐðĔĕÉéĘęĖėÈèËëÊêĒēĢģİiĮįÍíÎîÏïĪīĶķŁłŊŋŇňŃńŅņŠšŚśŞşŤťŦŧŢţŲųŪūÚúŮůÝýŽžŹźŻżÕõÔôÓóÖöÜüÄäŒœÆæØøÅå .'\\-/\n%§\\!?@_()+:;,=\"&]*$";
 
     private InputValideringRegex() {
         throw new IllegalAccessError("Skal ikke instansieres");
