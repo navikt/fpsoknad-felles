6d6f91f1727f81da54280df0f9efacc88948e5bd
Espen Waaga <espen.waaga@nav.no>
2022-03-04 10:35:44 +0100

    Oppdaterer inputvalideringregex for fritekst

--- src/main/java/no/nav/foreldrepenger/common/domain/validation/InputValideringRegex.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/validation/InputValideringRegex.java
@@ -2,7 +2,7 @@ package no.nav.foreldrepenger.common.domain.validation;
 
 public class InputValideringRegex {
 
-    public static final String FRITEKST = "^[0-9a-zA-ZÁáĄąÂâĀāĂăßČčĆćÇçĎďĐđÐðĔĕÉéĘęĖėÈèËëÊêĒēĢģİiĮįÍíÎîÏïĪīĶķŁłŊŋŇňŃńŅņÑñÞþŠšŚśŞşŤťŦŧŢţŲųŪūÚúŮůÝýŽžŹźŻżÕõÔôÓóÖöÜüÄäŒœÆæØøÅå .…'\\-/\n%§\\!?@_()+:;,=\"&]*$";
+    public static final String FRITEKST = "^[0-9a-zA-ZÁáĄąÂâĀāĂăßČčĆćÇçĎďĐđÐðĔĕÉéĘęĖėÈèËëÊêĒēĢģİiĮįÍíÎîÏïĪīĶķŁłŊŋŇňŃńŅņÑñÞþŠšŚśŞşŤťŦŧŢţŲųŪūÚúŮůÝýŽžŹźŻżÕõÔôÓóÖöÜüÄäŒœÆæØøÅå .…'\\-/\n%§\\!?@_()+:;,=\"&]*$";
 
     private InputValideringRegex() {
         throw new IllegalAccessError("Skal ikke instansieres");
