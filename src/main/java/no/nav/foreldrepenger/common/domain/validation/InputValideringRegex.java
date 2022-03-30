82041a02ce93664d959e5a2ca8b0ddea1654b2e8
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2022-03-30 10:39:28 +0200

    Legger til \ \n og \t i regex for inputvalidering (#71)
    * Utvider valideringsregex med følgende tillatte tegn \ \n \t –
    
    * Fikser antall \
    
    * Fikser regex for validering og legger til enhetstester
    
    * Legger til \ som tillatt regex

--- src/main/java/no/nav/foreldrepenger/common/domain/validation/InputValideringRegex.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/validation/InputValideringRegex.java
@@ -2,14 +2,15 @@ package no.nav.foreldrepenger.common.domain.validation;
 
 public class InputValideringRegex {
 
-    private static final String BOKSTAVER_OG_TALL = "0-9a-zA-ZÁáĄąÂâĀāĂăßČčĆćÇçĎďĐđÐðĔĕÉéĘęĖėÈèËëÊêĒēĢģİiĮįÍíÎîÏïĪīĶķŁłŊŋŇňŃńŅņÑñÞþŠšŚśŞşŤťŦŧŢţŲųŪūÚúŮůÝýŽžŹźŻżÕõÔôÓóÖöÜüÄäŒœÆæØøÅå";
-    private static final String FORVENTEDE_SPESIAL_TEGN_FRITEKST = " .…'\\-/\n%§\\!?@_()+:;,=\"&";
-    private static final String FORVENTEDE_SPESIAL_TEGN_JSON = "\\[\\]{}";
     private static final String START = "^[";
+    private static final String BOKSTAVER_OG_TALL = "0-9a-zA-ZÁáĄąÂâĀāĂăßČčĆćÇçĎďĐđÐðĔĕÉéĘęĖėÈèËëÊêĒēĢģİiĮįÍíÎîÏïĪīĶķŁłŊŋŇňŃńŅņÑñÞþŠšŚśŞşŤťŦŧŢţŲųŪūÚúŮůÝýŽžŹźŻżÕõÔôÓóÖöÜüÄäŒœÆæØøÅå";
+    // Følgende tegn må escapes med \\ i Java hvis de skal tillates (ellers kan de ha en annen betydning): <([{\\^-=$!|]})?*+.>
+    private static final String FORVENTEDE_SPESIAL_TEGN_FRITEKST = " …'/%§&@_:;," + "\\.\\!\\?\\(\\)\\+\\=\\-\\–\\*\"\r\n\t\\\\";
+    private static final String FORVENTEDE_SPESIAL_TEGN_JSON = "\\[\\{\\}\\]";
     private static final String SLUTT = "]*$";
 
     public static final String FRITEKST = START + BOKSTAVER_OG_TALL + FORVENTEDE_SPESIAL_TEGN_FRITEKST + SLUTT;
-    public static final String JSON_MED_FRITEKST = START + BOKSTAVER_OG_TALL + FORVENTEDE_SPESIAL_TEGN_FRITEKST + FORVENTEDE_SPESIAL_TEGN_JSON + SLUTT;
+    public static final String FRITEKST_JSON = START + BOKSTAVER_OG_TALL + FORVENTEDE_SPESIAL_TEGN_FRITEKST + FORVENTEDE_SPESIAL_TEGN_JSON + SLUTT;
 
     private InputValideringRegex() {
         throw new IllegalAccessError("Skal ikke instansieres");
---
+++ src/test/java/no/nav/foreldrepenger/common/domain/validation/InputValideringRegexTest.java
@@ -0,0 +1,93 @@
+package no.nav.foreldrepenger.common.domain.validation;
+
+import static no.nav.foreldrepenger.common.domain.validation.InputValideringRegex.FRITEKST;
+import static no.nav.foreldrepenger.common.domain.validation.InputValideringRegex.FRITEKST_JSON;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.Test;
+
+class InputValideringRegexTest {
+
+    @Test
+    void fritektsSkalVæreGyldigOgIkkeStoppesAvRegexen() {
+        var tekst = """
+                En helt vanlig tekts med litt linje skift \n
+                litt tab \t og \\n andre spesialtegn som ,.-_@41235()?...
+                2+2=4 mens 3-1=2. WOW! öäåØÆÅÉÜéü?
+                """;
+        assertThat(verifiser(FRITEKST, tekst)).isTrue();
+    }
+
+    @Test
+    void tekstSomInneholderUlovligTegnBlirAvvistAvRegex() {
+        var tekst = """
+                En helt vanlig tekts med litt linje skift,
+                men har følgende ulovlig tegn <>
+                """;
+        assertThat(verifiser(FRITEKST, tekst)).isFalse();
+    }
+
+    @Test
+    void tekstSomInneholderUlovligTegnBlirAvvistAvRegex2() {
+        var tekst = """
+                En helt vanlig tekts med litt linje skift,
+                men har følgende ulovlig tegn $
+                """;
+        assertThat(verifiser(FRITEKST, tekst)).isFalse();
+    }
+
+    @Test
+    void tekstSomInneholderUlovligTegnBlirAvvistAvRegex3() {
+        var tekst = """
+                En helt vanlig tekts med litt linje skift \n
+                litt tab \t og andre spesialtegn som ,.-_@41235()?...
+                MEN... Ulovlig tegn: {}
+                """;
+        assertThat(verifiser(FRITEKST, tekst)).isFalse();
+    }
+
+    @Test
+    void tekstSomInneholderUlovligTegnBlirAvvistAvRegex4() {
+        var tekst = """
+                En helt vanlig tekts med litt linje skift \n
+                litt tab \t og andre spesialtegn som ,.-_@41235()?...
+                MEN... Ulovlig tegn: |
+                """;
+        assertThat(verifiser(FRITEKST, tekst)).isFalse();
+    }
+
+
+    @Test
+    void jsonObjektSomSendesSomEnStrengSkalIkkeBlokkeresVedValidering() {
+        var tekst = """
+                {"søknad":{"tilleggsopplysninger":{"begrunnelseForSenEndring":{"tekst":"idas a\n\ndansa \nand
+                sdal ads\n\nsadlc\nl\nllcear   æø","ekstraInformasjon":"ARBEID"}}}}
+                "currentRoute": "/soknad/uttaksplan-info"  {} ,.,++ë@hotmail.no--Sopra/
+                {"søknad":{"type":"foreldrepenger","harGodkjentVilkår":true,"søkersituasjon":{"situasjon":"adopsjon",
+                {"tekst":"idas a\n\ndansa \nand sdal \sads\n\nsadlc\nl\nllcear   æø","ekstraInformasjon":"ARBEID"}}}
+                ,"version":3,"currentRoute":"/soknad/utenlandsopphold","uttaksplanInfo":{"harAnnenForelderSøktFP":true,
+                """;
+        assertThat(verifiser(FRITEKST_JSON, tekst)).isTrue();
+    }
+
+    @Test
+    void jsonObjektSomSendesSomEnStrengSkalBlokkeresVedUlovligTegn() {
+        var tekst = """
+                {
+                    "body": "litt Lovlig Tekst\n Hva med noe",
+                    "bodyUlovlig": "superDuper\\^"
+                    }
+                """;
+        assertThat(verifiser(FRITEKST_JSON, tekst)).isFalse();
+    }
+
+
+
+    private boolean verifiser(String regex, String verdi) {
+        var pattern = Pattern.compile(regex);
+        return pattern.matcher(verdi).matches();
+    }
+
+}
