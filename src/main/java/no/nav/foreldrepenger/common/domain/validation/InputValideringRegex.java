d1838a63d0876a33f34a67e10a5497db90e8f57d
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2022-04-08 11:36:01 +0200

    Legger til ´ i regex for fritekst (#77)

--- src/main/java/no/nav/foreldrepenger/common/domain/validation/InputValideringRegex.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/validation/InputValideringRegex.java
@@ -7,7 +7,7 @@ public class InputValideringRegex {
     public static final String BOKSTAVER_MED_MELLOMROM = "^[\\p{L}\\p{Space}]*$";
     public static final String NORSK_FØDSELSNUMMER = "^[\\p{Digit}]{11}+$";
     public static final String ORGNUMMER = "^[\\p{Digit}]{9}+$";
-    public static final String FRITEKST = "^[\\p{Punct}\\p{L}\\p{M}\\p{N}\\p{Sc}\\p{Space}\\–…§]*$";
+    public static final String FRITEKST = "^[\\p{Punct}\\p{L}\\p{M}\\p{N}\\p{Sc}\\p{Space}\\–…§´]*$";
 
     private InputValideringRegex() {
         throw new IllegalAccessError("Skal ikke instansieres");
