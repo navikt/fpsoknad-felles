2e0085f83cde4e006aa60db145354af40c670a72
Espen Waaga <espen.waaga@nav.no>
2022-05-30 10:11:27 +0200

    Expand contract på enum også: Legger til @JsonAlias på EttersendingsType

--- src/main/java/no/nav/foreldrepenger/common/domain/felles/EttersendingsType.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/felles/EttersendingsType.java
@@ -1,8 +1,12 @@
 package no.nav.foreldrepenger.common.domain.felles;
 
-public enum EttersendingsType {
-    ENGANGSSTØNAD,
-    FORELDREPENGER,
-    SVANGERSKAPSPENGER
+import com.fasterxml.jackson.annotation.JsonAlias;
 
+public enum EttersendingsType {
+    @JsonAlias("engangsstønad")
+    ENGANGSSTØNAD,
+    @JsonAlias("foreldrepenger")
+    FORELDREPENGER,
+    @JsonAlias("svangerskapspenger")
+    SVANGERSKAPSPENGER,
 }
