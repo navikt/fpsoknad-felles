539ed45afa60aabdd1a5afb83f381e71b7c61299
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2022-08-15 14:23:05 +0200

    Expand-contract for nytt/endret felt (#115)

--- src/main/java/no/nav/foreldrepenger/common/domain/foreldrepenger/Rettigheter.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/foreldrepenger/Rettigheter.java
@@ -1,10 +1,13 @@
 package no.nav.foreldrepenger.common.domain.foreldrepenger;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 import no.nav.foreldrepenger.common.domain.validation.annotations.Rettighet;
 
 @Rettighet
 public record Rettigheter(boolean harAnnenForelderRett,
                           boolean harAleneOmsorgForBarnet,
                           boolean harMorUføretrygd,
+                          @JsonAlias("harMorForeldrepengerEØS") // Fjern etter at både api og mottak er oppdatert
                           boolean harRettPåForeldrepengerIEØS) {
 }
