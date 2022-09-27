05287df7b23b11492e8138c8ee1cbed2d843117e
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-09-27 13:15:53 +0200

    Endrer felter fra boolean til Boolean for å kommunisere hvorvidt bruker har svart på dette i frontend

--- src/main/java/no/nav/foreldrepenger/common/domain/foreldrepenger/Rettigheter.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/foreldrepenger/Rettigheter.java
@@ -1,13 +1,10 @@
 package no.nav.foreldrepenger.common.domain.foreldrepenger;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
-
 import no.nav.foreldrepenger.common.domain.validation.annotations.Rettighet;
 
 @Rettighet
 public record Rettigheter(boolean harAnnenForelderRett,
                           boolean harAleneOmsorgForBarnet,
-                          boolean harMorUføretrygd,
-                          @JsonAlias("harAnnenPartRettPåForeldrepengerIEØS") // Fjern etter at både api og mottak er oppdatert
-                          boolean harAnnenForelderTilsvarendeRettEØS) {
+                          Boolean harMorUføretrygd,
+                          Boolean harAnnenForelderTilsvarendeRettEØS) {
 }
