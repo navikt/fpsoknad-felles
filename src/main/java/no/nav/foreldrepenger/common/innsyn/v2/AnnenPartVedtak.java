62a6b9a654eed99d36f0226b226b781d9d422ba7
Anders Palfi <anderspalfi@gmail.com>
2023-01-25 15:08:40 +0100

    Antall barn i annen parts vedtak

--- src/main/java/no/nav/foreldrepenger/common/innsyn/v2/AnnenPartVedtak.java
+++ src/main/java/no/nav/foreldrepenger/common/innsyn/v2/AnnenPartVedtak.java
@@ -3,5 +3,5 @@ package no.nav.foreldrepenger.common.innsyn.v2;
 import java.time.LocalDate;
 import java.util.List;
 
-public record AnnenPartVedtak(List<UttakPeriode> perioder, LocalDate termindato, Dekningsgrad dekningsgrad) {
+public record AnnenPartVedtak(List<UttakPeriode> perioder, LocalDate termindato, Dekningsgrad dekningsgrad, int antallBarn) {
 }
