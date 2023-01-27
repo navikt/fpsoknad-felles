416b931f5e94da190a23a0a14a906d1fb0384f99
Anders Palfi <anderspalfi@gmail.com>
2023-01-27 09:55:05 +0100

    Behandlingstilstand vent på meldekort

--- src/main/java/no/nav/foreldrepenger/common/innsyn/v2/BehandlingTilstand.java
+++ src/main/java/no/nav/foreldrepenger/common/innsyn/v2/BehandlingTilstand.java
@@ -1,5 +1,5 @@
 package no.nav.foreldrepenger.common.innsyn.v2;
 
 public enum BehandlingTilstand {
-    UNDER_BEHANDLING, VENT_TIDLIG_SØKNAD
+    UNDER_BEHANDLING, VENT_TIDLIG_SØKNAD, VENT_PÅ_MELDEKORT
 }
