303d80e67ec26a40aef034cf38b508856df58f69
espenwaaga <espen.waaga@nav.no>
2023-02-24 11:05:25 +0100

    AnnenForelder kan være records

--- src/main/java/no/nav/foreldrepenger/common/domain/felles/annenforelder/NorskForelder.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/felles/annenforelder/NorskForelder.java
@@ -1,30 +1,16 @@
 package no.nav.foreldrepenger.common.domain.felles.annenforelder;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import no.nav.foreldrepenger.common.domain.Fødselsnummer;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import java.util.Objects;
 import java.util.Optional;
 
 import static no.nav.foreldrepenger.common.domain.validation.InputValideringRegex.FRITEKST;
 import static no.nav.foreldrepenger.common.util.StringUtil.mask;
 
-public final class NorskForelder implements AnnenForelder {
-
-    @Valid
-    @NotNull
-    private final Fødselsnummer fnr;
-    @Pattern(regexp = FRITEKST)
-    private final String navn;
-
-    @JsonCreator
-    public NorskForelder(Fødselsnummer fnr, String navn) {
-        this.fnr = fnr;
-        this.navn = navn;
-    }
+public record NorskForelder(@Valid @NotNull Fødselsnummer fnr, @Pattern(regexp = FRITEKST) String navn) implements AnnenForelder {
 
     @Override
     public boolean hasId() {
@@ -33,31 +19,11 @@ public final class NorskForelder implements AnnenForelder {
                 .isPresent();
     }
 
-    public Fødselsnummer getFnr() {
-        return fnr;
-    }
-
-    public String getNavn() {
-        return navn;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        NorskForelder that = (NorskForelder) o;
-        return Objects.equals(fnr, that.fnr);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(fnr);
-    }
-
     @Override
     public String toString() {
-        return "NorskForelder{" + "fnr=" + fnr + ", navn='" + mask(navn) + '\'' + "} " + super.toString();
+        return "NorskForelder{" +
+                "fnr=" + fnr +
+                ", navn='" + mask(navn) + '\'' +
+                '}';
     }
 }
--- src/main/java/no/nav/foreldrepenger/common/domain/felles/annenforelder/UtenlandskForelder.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/felles/annenforelder/UtenlandskForelder.java
@@ -5,65 +5,26 @@ import com.neovisionaries.i18n.CountryCode;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import java.util.Objects;
 
 import static no.nav.foreldrepenger.common.domain.validation.InputValideringRegex.FRITEKST;
 import static no.nav.foreldrepenger.common.util.StringUtil.mask;
 
 
-public final class UtenlandskForelder implements AnnenForelder {
-
-    @NotBlank
-    @Pattern(regexp = FRITEKST)
-    private final String id;
-    @NotNull
-    private final CountryCode land;
-    @Pattern(regexp = FRITEKST)
-    private final String navn;
-
-    public UtenlandskForelder(String id, CountryCode land, String navn) {
-        this.id = id;
-        this.land = land;
-        this.navn = navn;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public CountryCode getLand() {
-        return land;
-    }
-
-    public String getNavn() {
-        return navn;
-    }
+public record UtenlandskForelder(@NotBlank @Pattern(regexp = FRITEKST) String id,
+                                 @NotNull CountryCode land,
+                                 @Pattern(regexp = FRITEKST) String navn) implements AnnenForelder {
 
     @Override
     public boolean hasId() {
         return id != null;
     }
 
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        UtenlandskForelder that = (UtenlandskForelder) o;
-        return Objects.equals(id, that.id) &&
-                land == that.land &&
-                Objects.equals(navn, that.navn);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, land, navn);
-    }
-
     @Override
     public String toString() {
-        return "UtenlandskForelder{" + "id='" + mask(id) + '\'' + ", land=" + land + ", navn='" + mask(navn) + '\'' + "} " + super.toString();
+        return "UtenlandskForelder{" +
+                "id='" + mask(id) + '\'' +
+                ", land=" + land +
+                ", navn='" + mask(navn) + '\'' +
+                '}';
     }
 }
