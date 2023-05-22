498cdba26f3433a8847c379ba2ec356c3ba72267
espenwaaga <espen.waaga@nav.no>
2023-05-22 15:14:28 +0200

    Fjerner unødvendig overriding av getter for jackson deseralisering for single-value-objects

--- src/main/java/no/nav/foreldrepenger/common/domain/Fødselsnummer.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/Fødselsnummer.java
@@ -5,11 +5,10 @@ import static no.nav.foreldrepenger.common.util.StringUtil.mask;
 
 import java.util.Objects;
 
-import jakarta.validation.constraints.Pattern;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
+import jakarta.validation.constraints.Pattern;
 import no.nav.foreldrepenger.common.domain.felles.Kjønn;
 
 public record Fødselsnummer(@Pattern(regexp = FRITEKST) @JsonValue String value) {
@@ -24,11 +23,6 @@ public record Fødselsnummer(@Pattern(regexp = FRITEKST) @JsonValue String value
             return value.charAt(8) % 2 == 0 ? Kjønn.K : Kjønn.M;
         }
         return Kjønn.U;
-    }
-
-    @Override
-    public String value() { // NOSONAR: Her overrider vi default getter fra record fordi den propagerer annoteringer fra field. Vi ønsker ikke @JsonValue på getter.
-        return value;
     }
 
     @Override
--- src/main/java/no/nav/foreldrepenger/common/innsyn/Saksnummer.java
+++ src/main/java/no/nav/foreldrepenger/common/innsyn/Saksnummer.java
@@ -11,9 +11,4 @@ public record Saksnummer(@JsonValue String value) {
     public Saksnummer {
         Objects.requireNonNull(value, "saksnummer kan ikke være null");
     }
-
-    @Override
-    public String value() { // NOSONAR: Her overrider vi default getter fra record fordi den propagerer annoteringer fra field. Vi ønsker ikke @JsonValue på getter.
-        return value;
-    }
 }
