c59047a91887ad8c25081e1a53f379d48d8b065b
espenwaaga <espen.waaga@nav.no>
2023-05-24 15:18:47 +0200

    Gjør DTO kompatible med både jackson 2.14 og 2.15

--- src/main/java/no/nav/foreldrepenger/common/domain/Orgnummer.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/Orgnummer.java
@@ -3,6 +3,7 @@ package no.nav.foreldrepenger.common.domain;
 import static no.nav.foreldrepenger.common.domain.validation.InputValideringRegex.FRITEKST;
 import static no.nav.foreldrepenger.common.util.StringUtil.mask;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 import jakarta.validation.constraints.Pattern;
@@ -11,6 +12,10 @@ import no.nav.foreldrepenger.common.domain.validation.annotations.Orgnr;
 public record Orgnummer(@JsonValue @Pattern(regexp = FRITEKST) @Orgnr String value) implements ArbeidsgiverIdentifikator {
     public static final String MAGIC = "342352362";
     public static final Orgnummer MAGIC_ORG = new Orgnummer(MAGIC);
+
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    public Orgnummer { // NOSONAR
+    }
 
     public String maskert() {
         return value != null ? mask(value) : "<null>";
--- src/main/java/no/nav/foreldrepenger/common/domain/felles/VedleggReferanse.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/felles/VedleggReferanse.java
@@ -1,5 +1,6 @@
 package no.nav.foreldrepenger.common.domain.felles;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 import jakarta.validation.constraints.NotNull;
@@ -7,4 +8,7 @@ import jakarta.validation.constraints.Pattern;
 
 public record VedleggReferanse(@NotNull @Pattern(regexp = "^[\\p{Digit}\\p{L}-_]*$") @JsonValue String referanse) {
 
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    public VedleggReferanse { // NOSONAR
+    }
 }
