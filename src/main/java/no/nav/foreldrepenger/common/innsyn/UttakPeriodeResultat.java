9ae1c0c44c6a5c9d581521c2fa8dbcdae9e3ad76
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2023-05-30 12:34:55 +0200

    Legger til spesiell equalsmetode for trekkerMinsterett (#188)

--- src/main/java/no/nav/foreldrepenger/common/innsyn/UttakPeriodeResultat.java
+++ src/main/java/no/nav/foreldrepenger/common/innsyn/UttakPeriodeResultat.java
@@ -1,10 +1,38 @@
 package no.nav.foreldrepenger.common.innsyn;
 
+import java.util.Objects;
+
 public record UttakPeriodeResultat(boolean innvilget, boolean trekkerMinsterett, boolean trekkerDager, Årsak årsak) {
 
     public enum Årsak {
         ANNET,
         AVSLAG_HULL_MELLOM_FORELDRENES_PERIODER,
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UttakPeriodeResultat that = (UttakPeriodeResultat) o;
+        return innvilget == that.innvilget &&
+                erTrekkerMinstrettLikNårTrekkdagerErTrue(that) &&
+                trekkerDager == that.trekkerDager &&
+                årsak == that.årsak;
+    }
+
+    // TODO: Fjern etter sammenligning!
+    private boolean erTrekkerMinstrettLikNårTrekkdagerErTrue(UttakPeriodeResultat that) {
+        if (!trekkerDager || !that.trekkerDager) {
+            return true;
+        }
+        return trekkerMinsterett == that.trekkerMinsterett;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(innvilget, trekkerMinsterett, trekkerDager, årsak);
     }
 }
 
