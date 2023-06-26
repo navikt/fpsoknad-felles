e11a33b62382a9ffd995f6193c0236f842ba047d
anjaaalerud <anja.gotesen.aalerud@nav.no>
2023-06-26 10:44:44 +0200

    Lagt til Oppholdkilde for OppholdPeriode for svp for å kunne gi bruker informasjon om hvor oppholdet kommer fra

--- src/main/java/no/nav/foreldrepenger/common/innsyn/svp/OppholdPeriode.java
+++ src/main/java/no/nav/foreldrepenger/common/innsyn/svp/OppholdPeriode.java
@@ -2,9 +2,13 @@ package no.nav.foreldrepenger.common.innsyn.svp;
 
 import java.time.LocalDate;
 
-public record OppholdPeriode(LocalDate fom, LocalDate tom, Årsak årsak) {
+public record OppholdPeriode(LocalDate fom, LocalDate tom, Årsak årsak, OppholdKilde oppholdKilde) {
     public enum Årsak {
         SYKEPENGER,
         FERIE
+    }
+    public enum OppholdKilde {
+        SAKSBEHANDLER,
+        INNTEKTSMELDING
     }
 }
