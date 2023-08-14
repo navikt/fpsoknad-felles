8e257b5260854567f958d9550fb8627b576d16cc
Anders Palfi <anderspalfi@gmail.com>
2023-08-14 13:52:26 +0200

    Utvider vedtak med periodeårsak om fratrekk pga pleiepenger (#212)

--- src/main/java/no/nav/foreldrepenger/common/innsyn/UttakPeriodeResultat.java
+++ src/main/java/no/nav/foreldrepenger/common/innsyn/UttakPeriodeResultat.java
@@ -1,38 +1,11 @@
 package no.nav.foreldrepenger.common.innsyn;
 
-import java.util.Objects;
-
 public record UttakPeriodeResultat(boolean innvilget, boolean trekkerMinsterett, boolean trekkerDager, Årsak årsak) {
 
     public enum Årsak {
         ANNET,
         AVSLAG_HULL_MELLOM_FORELDRENES_PERIODER,
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        UttakPeriodeResultat that = (UttakPeriodeResultat) o;
-        return innvilget == that.innvilget &&
-                erTrekkerMinstrettLikNårTrekkdagerErTrue(that) &&
-                trekkerDager == that.trekkerDager &&
-                årsak == that.årsak;
-    }
-
-    // TODO: Fjern etter sammenligning!
-    private boolean erTrekkerMinstrettLikNårTrekkdagerErTrue(UttakPeriodeResultat that) {
-        if (!trekkerDager) {
-            return true;
-        }
-        return trekkerMinsterett == that.trekkerMinsterett;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(innvilget, trekkerDager, årsak);
+        AVSLAG_FRATREKK_PLEIEPENGER,
     }
 }
 
