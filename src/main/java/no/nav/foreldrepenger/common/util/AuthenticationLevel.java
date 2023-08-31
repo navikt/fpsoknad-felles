92d6cb0e52b27a9e06bb1cf3cf7596def11a1c0e
Michal J. Sladek <mrsladek@users.noreply.github.com>
2023-08-31 13:45:20 +0200

    Legger inn idporten som issuer og utvider aksepterte AuthenticationLevels (#216)

--- src/main/java/no/nav/foreldrepenger/common/util/AuthenticationLevel.java
+++ src/main/java/no/nav/foreldrepenger/common/util/AuthenticationLevel.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 
 public enum AuthenticationLevel {
     NONE,
+    IDPORTEN_LOA_SUBSTANTIAL,
+    IDPORTEN_LOA_HIGH,
     LEVEL3,
     LEVEL4;
 
     public static AuthenticationLevel of(String level) {
         return Arrays.stream(values())
-                .filter(v -> v.name().equalsIgnoreCase(level))
+                .filter(v -> v.name().replace("_", "-").equalsIgnoreCase(level))
                 .findFirst()
                 .orElse(NONE);
     }
