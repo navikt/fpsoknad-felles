cd83be703d6b186f2c1540ae8ff2983353ac6d3f
Anders Palfi <anderspalfi@gmail.com>
2024-09-09 09:42:53 +0200

    Metode for equals for UttakPeriode som ekskluderer tidsperiode, tiltenkt bruk fpoversikt for å slå sammen perioder (#345)

--- src/main/java/no/nav/foreldrepenger/common/innsyn/UttakPeriode.java
+++ src/main/java/no/nav/foreldrepenger/common/innsyn/UttakPeriode.java
@@ -1,6 +1,7 @@
 package no.nav.foreldrepenger.common.innsyn;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public record UttakPeriode(LocalDate fom,
                            LocalDate tom,
@@ -13,4 +14,11 @@ public record UttakPeriode(LocalDate fom,
                            MorsAktivitet morsAktivitet,
                            SamtidigUttak samtidigUttak,
                            boolean flerbarnsdager) {
+
+    public boolean likBortsattFraTidsperiode(UttakPeriode that) { //Brukes for å slå sammen like perioder
+        return flerbarnsdager == that.flerbarnsdager && kontoType == that.kontoType && Objects.equals(gradering, that.gradering)
+                && oppholdÅrsak == that.oppholdÅrsak && morsAktivitet == that.morsAktivitet && Objects.equals(samtidigUttak,
+                that.samtidigUttak) && Objects.equals(resultat, that.resultat) && utsettelseÅrsak == that.utsettelseÅrsak
+                && overføringÅrsak == that.overføringÅrsak;
+    }
 }
