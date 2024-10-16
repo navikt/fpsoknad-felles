f11852a4cb55b6c3f5aeccfb41a0c5cd932be558
Steffen Pøhner <steffen.h.pohner@nav.no>
2024-10-16 11:59:42 +0200

    Infoskriv til arbeidsgiver behøves ikke lenger. Fjerner derfor. (POFIM-59)

--- src/main/java/no/nav/foreldrepenger/common/domain/Kvittering.java
+++ src/main/java/no/nav/foreldrepenger/common/domain/Kvittering.java
@@ -10,21 +10,20 @@ import java.util.Objects;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 @JsonInclude(NON_NULL)
-public record Kvittering(LocalDateTime mottattDato, @Valid Saksnummer saksNr, byte[] pdf, byte[] infoskrivPdf) {
+public record Kvittering(LocalDateTime mottattDato, @Valid Saksnummer saksNr, byte[] pdf) {
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Kvittering that = (Kvittering) o;
-        return Objects.equals(mottattDato, that.mottattDato) && Objects.equals(saksNr, that.saksNr) && Arrays.equals(pdf, that.pdf) && Arrays.equals(infoskrivPdf, that.infoskrivPdf);
+        return Objects.equals(mottattDato, that.mottattDato) && Objects.equals(saksNr, that.saksNr) && Arrays.equals(pdf, that.pdf);
     }
 
     @Override
     public int hashCode() {
         int result = Objects.hash(mottattDato, saksNr);
         result = 31 * result + Arrays.hashCode(pdf);
-        result = 31 * result + Arrays.hashCode(infoskrivPdf);
         return result;
     }
 
