0a0ca11dea6ab1877ee5e03cbed9ec8d2ba0a415
johannbm <johannes.moskvil@gmail.com>
2025-03-26 08:48:27 +0100

    ekstra annotasjoner

--- src/main/java/no/nav/foreldrepenger/common/innsyn/FpVedtak.java
+++ src/main/java/no/nav/foreldrepenger/common/innsyn/FpVedtak.java
@@ -1,7 +1,8 @@
 package no.nav.foreldrepenger.common.innsyn;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
-public record FpVedtak(@NotNull List<UttakPeriode> perioder) { }
+public record FpVedtak(@NotNull List<@NotNull @Valid UttakPeriode> perioder) { }
