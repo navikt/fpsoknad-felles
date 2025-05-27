b5484e5c6333713b48c64898fdbe3c3f25038b1b
Johannes Moskvil <johannbm@users.noreply.github.com>
2025-05-27 12:53:52 +0200

    Annoter flere @NotNull på innsyn DTOs (#425)

--- src/main/java/no/nav/foreldrepenger/common/innsyn/svp/OppholdPeriode.java
+++ src/main/java/no/nav/foreldrepenger/common/innsyn/svp/OppholdPeriode.java
@@ -1,8 +1,10 @@
 package no.nav.foreldrepenger.common.innsyn.svp;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
-public record OppholdPeriode(LocalDate fom, LocalDate tom, Årsak årsak, OppholdKilde oppholdKilde) {
+public record OppholdPeriode(@NotNull LocalDate fom, @NotNull LocalDate tom, @NotNull Årsak årsak, @NotNull OppholdKilde oppholdKilde) {
     public enum Årsak {
         SYKEPENGER,
         FERIE
--- src/main/java/no/nav/foreldrepenger/common/innsyn/svp/Tilrettelegging.java
+++ src/main/java/no/nav/foreldrepenger/common/innsyn/svp/Tilrettelegging.java
@@ -2,10 +2,11 @@ package no.nav.foreldrepenger.common.innsyn.svp;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotNull;
 import no.nav.foreldrepenger.common.innsyn.Arbeidstidprosent;
 
-public record Tilrettelegging(LocalDate fom,
-                              LocalDate tom,
+public record Tilrettelegging(@NotNull LocalDate fom,
+                              @NotNull LocalDate tom,
                               TilretteleggingType type,
                               Arbeidstidprosent arbeidstidprosent,
                               PeriodeResultat resultat) {
