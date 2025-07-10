6e6b95e92069de3ce602cf79f2a1cb4bd8ad0688
espenwaaga <espen.waaga@nav.no>
2025-07-10 13:04:18 +0200

    Legger til flere valideringer for trekkdager

--- src/main/java/no/nav/foreldrepenger/common/innsyn/UttakPeriodeAnnenpartEøs.java
+++ src/main/java/no/nav/foreldrepenger/common/innsyn/UttakPeriodeAnnenpartEøs.java
@@ -3,10 +3,13 @@ package no.nav.foreldrepenger.common.innsyn;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record UttakPeriodeAnnenpartEøs(@NotNull LocalDate fom,
                                        @NotNull LocalDate tom,
                                        @NotNull KontoType kontoType,
-                                       @NotNull BigDecimal trekkdager) {
+                                       @NotNull @Min(0) @Max(1000) @Digits(integer = 3, fraction = 2) BigDecimal trekkdager) {
 }
