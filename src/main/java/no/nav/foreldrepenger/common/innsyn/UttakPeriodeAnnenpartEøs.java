975784c6f6a6815902248cb5495e1650ff0973cb
espenwaaga <espen.waaga@nav.no>
2025-08-14 12:36:59 +0200

    Trekkdager value type

--- src/main/java/no/nav/foreldrepenger/common/innsyn/UttakPeriodeAnnenpartEøs.java
+++ src/main/java/no/nav/foreldrepenger/common/innsyn/UttakPeriodeAnnenpartEøs.java
@@ -3,6 +3,9 @@ package no.nav.foreldrepenger.common.innsyn;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -11,5 +14,8 @@ import jakarta.validation.constraints.NotNull;
 public record UttakPeriodeAnnenpartEøs(@NotNull LocalDate fom,
                                        @NotNull LocalDate tom,
                                        @NotNull KontoType trekkonto,
-                                       @NotNull @Min(0) @Max(1000) @Digits(integer = 3, fraction = 1) BigDecimal trekkdager) {
+                                       @Valid @NotNull Trekkdager trekkdager) {
+
+    public record Trekkdager(@Min(0) @Max(1000) @Digits(integer = 3, fraction = 1) @JsonValue BigDecimal trekkdager) {
+    }
 }
